6b560c3d7a906f0cdcc384391de7967783f8ee33
fjdcordon <105855130+fjdcordon@users.noreply.github.com>
2022-06-24 12:11:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 echo gateway > /etc/hostname
 userdel -r vagrant
 cp driver gateway.py protocols.py /root
+chmod +x /root/driver
 pip install scapy
 cp gtw.service /etc/systemd/system
 systemctl enable gtw
